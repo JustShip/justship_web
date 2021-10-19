b2356b327adc12b714fc9835d3c2b9db860176f7
Carlos Lugones <contact@lugodev.com>
2021-10-19 01:28:18 -0400

    chore: vuex

--- nuxt.config.js
+++ nuxt.config.js
@@ -84,6 +84,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/vuex' },
     { src: '~/plugins/vuelidate' }
   ],
 
