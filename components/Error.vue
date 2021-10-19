840bcfa346c5e8e2ad607a5b554c9eae3991130e
Carlos Lugones <contact@lugodev.com>
2021-10-19 00:51:43 -0400

    feat: error component

---
+++ components/Error.vue
@@ -0,0 +1,20 @@
+<template>
+  <transition name="fade">
+    <p
+      :class="`${ {transitioned: 'ease'} }`"
+    >
+      <slot />
+    </p>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: {
+    transitioned: {
+      type: Boolean,
+      default: true,
+    },
+  },
+}
+</script>
